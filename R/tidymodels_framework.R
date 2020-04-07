8ca21576c3d1ddfd8092deba15f30ebc8e4970e1
JDO <atharos@vivaldi.net>
2020-04-07 13:26:42 +0100

    Expanding Predictions wrapper with data splitting and preprocessing

--- R/tidymodels_framework.R
+++ R/tidymodels_framework.R
@@ -16,6 +16,40 @@
 #' @export
 #'
 #' @examples
-smTidymodelsPreditions <- function(.data, .target, .metrics = c("RMSE", "LLoss")){
+smTidymodelsPreditions <- function(.data, .target, .metrics = c("RMSE", "LLoss"), .split_prop = 0.6){
 
-}
+        ###### Defining Names and Data groups ######
+        split_data <-  initial_split(.data, prop = .split_prop)
+        .target <- enquo(.target)
+        target_name <- quo_name(.target)
+        data_colnames <- colnames(.data)
+        role_colnames <- if_else(data_colnames == target_name, "outcome", "predictor")
+
+        ###### Pre-Processing ######
+        core_recipe <- split_data %>%
+                training() %>%
+                recipe(vars = data_colnames, roles = role_colnames) %>%
+                step_corr(all_predictors()) %>%
+                prep()
+
+        training_data <- core_recipe %>%
+                juice()
+
+        testing_data <- split_data %>%
+                testing() %>%
+                {bake(core_recipe, .)}
+
+        ##### Model Training ######
+
+
+
+
+
+
+        ##### Output ######
+
+        output <- list("test_set" = testing_data, "training_set" = training_data)
+
+        output
+
+        }
